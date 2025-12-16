94183ae933ff2b5414ebfd17a90d2fe66289f5ee
rajan7design <rajanpaudel6789@gmail.com>
2025-12-16 16:37:12 +0545

    first commit

--- Number_Guess/Guess.java
+++ Number_Guess/Guess.java
@@ -1,35 +1,44 @@
 import java.util.Scanner;
- class GS {
+  public class Guess {
     int number ;
-    GS( int number ) {
+
+    Guess( int number ) {
         this.number= number ;
     }    
-}
 
-void guess (){
-      Scanner scanner=new Scanner(System.in);
-          int number ;
+
+void check (){
+      Scanner sc = new Scanner(System.in);
+          
       while (true) {
+
     System.out.println(" Enter your guess ");
-    int n = scanner.nextInt();
+    int n = sc.nextInt();
     if ( n == number ) {
         System.out.println( " Your guess is correct");
     }
-    else {
-        System.out.println( " Your guess is incorrect ");
-    
+    else if ( n > number ) {
+           System.out.println( " Your guess  is greater");
+
+        
     }
-
-
+    
+    else {
+        System.out.println( " Your guess is smaller  ");
+        
+    }
+        
+ 
       }
+      sc.close();
 }
-public class Guess {
+
     public static void main(String[] args) {
        
         
-
+        Guess  g = new Guess( 13) ;
          
-         
+         g .check();
 
        
     }
