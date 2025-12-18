7189af2b00d54e38d4963eaeae8f0bf34c4da89d
rajan7design <rajanpaudel6789@gmail.com>
2025-12-18 11:51:18 +0545

    t

---
+++ Day_3/Employee.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Employee {
+
+    int salary;
+     boolean check = true;
+
+    void getSalary() {
+        Scanner sc = new Scanner(System.in);
+        while (check) {
+            
+           
+        System.out.print("Enter your salary: ");
+        salary = sc.nextInt();
+
+        if (salary >= 30000) {
+            System.out.println("Your bonus is 10%");
+        } else {
+            System.out.println("Your bonus is 5%");
+        }
+        check=false;
+        sc.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.getSalary();
+    }
+
+}
+
