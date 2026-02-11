2ea6395b494cea685d1126d960902271e558dedc
Rajan Paudel <rajanpaudel6789@gmail.com>
2026-02-11 08:43:33 +0545

    matrix subtraction

---
+++ Day_90/Subtraction.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int matrix1[][] = new int[2][2];
+        int matrix2[][] = new int[2][2];
+
+        System.out.println("Enter 4 numbers for first 2x2 matrix:");
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                matrix1[i][j] = sc.nextInt();
+
+        System.out.println("Enter 4 numbers for second 2x2 matrix:");
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                matrix2[i][j] = sc.nextInt();
+
+        System.out.println("Matrix after subtraction:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++)
+                System.out.println(matrix1[i][j] - matrix2[i][j] );
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
